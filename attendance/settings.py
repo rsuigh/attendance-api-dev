44bfb222e3c27415d4706ebc5fcd293c43adee91
Rodrigo Suigh <suigh.rodrigo@gmail.com>
2024-06-18 10:14:29 -0300

    added localhost:8080 to cors

--- attendance/settings.py
+++ attendance/settings.py
@@ -93,7 +93,7 @@ ALLOWED_HOSTS = ['*']
 #CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://apps.local.edly.io:2029",
-    "http://localhost:8080/",
+    "http://localhost:8080",
 ]
 
 REST_FRAMEWORK = {
