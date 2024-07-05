9c127e9d80ae71ff71b8875ad684a154378a14ab
Rodrigo Suigh <suigh.rodrigo@gmail.com>
2024-07-05 09:46:22 -0300

    definitive version? surely no, but is a very good version

--- api/management/commands/export_attendance.py
+++ api/management/commands/export_attendance.py
@@ -7,6 +7,9 @@ import csv
 class Command(BaseCommand):
     '''Export attendance data to CSV'''
     def handle(self, *args, **kwargs):
+        attendance_records = AttendanceRecorder.objects.all()
+        student_attendance = defaultdict(lambda: defaultdict(dict))
+        dates_classes = set()
         for record in attendance_records:
             user = record.user
             date = record.date
@@ -16,7 +19,8 @@ class Command(BaseCommand):
                 present = attendance["present"]
                 student_attendance[(user, username)][f"{date}+{class_type}"] = 1 if present else 0
                 dates_classes.add(f"{date}+{class_type}")
-
+        sorted_dates_classes = sorted(dates_classes)
+        headers = ["user", "username"] + sorted_dates_classes
         with open('attendance_report.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter='|')
             writer.writerow(headers)
