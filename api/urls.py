863f4d3ddca9233a85d6d1bdcf6ff7f8a6c564d2
Rodrigo Suigh <suigh.rodrigo@gmail.com>
2024-07-18 11:07:47 -0300

    removed wrong imports

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AttendanceRecorderListAPIView, AttendanceHistoryListAPIView
+from .views import AttendanceRecorderListAPIView
 
 urlpatterns = [
     path('attendance/', AttendanceRecorderListAPIView.as_view(), name='attendance_list'),
